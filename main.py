7d7d85d3f3fb9a99ab6a9443fe7b3165bf0d4e3f
0Maan0 <maan.scipio@student.uva.nl>
2025-06-20 17:42:48 +0200

    some runs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.run_model_utils import *
 if __name__ == "__main__":
     # Model parameters
     num_agents = 10  # Number of scientists in the network
-    network_type = "cycle"  # Options: 'cycle', 'wheel', 'complete'
+    network_type = "complete"  # Options: 'cycle', 'wheel', 'complete'
     true_probs = (0.2, 0.8)  # Probabilities of the two theories being true (old, new)
     believe_strength_range = (0.5, 2.0)  # Range for belief strength of agents
     prior_strength_range = (1, 4)  # Range for prior strength of agents
