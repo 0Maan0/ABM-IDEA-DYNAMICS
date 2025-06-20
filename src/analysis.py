f7529f6821d6a62fd065e1ec0e67297b33661451
fredmloth <frederiekeloth@gmail.com>
2025-06-20 15:15:18 +0200

    started analysis file

---
+++ src/analysis.py
@@ -0,0 +1,21 @@
+""" Runs Analysis on the model based on research paper: 
+The Epistemic Benefit of Transient Diversity """
+
+def prob_succesful_learning(data):
+    """ Probability of succesful learning vs size 
+    compares cycle, wheel and complete. draws a line between dots on the graph.
+    
+    what is defined as succesful learning? - Every agent converging to the one method?
+    - which one?
+        new (aanname)?
+        based on true prob? but is that correct?
+        paper converges prob of correct theory. We need to define the correct theory.
+        kans op convergeren naar nieuwe theory 
+    """
+
+    # get the csv file
+
+    # save neccesary data from csv file
+
+    pass
+
