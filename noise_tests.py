1f4076b0959f002495b5a6dd03f00076ca56c4c6
fredmloth <frederiekeloth@gmail.com>
2025-06-24 17:10:49 +0200

    start file to test effect of noise

---
+++ noise_tests.py
@@ -0,0 +1,6 @@
+""" This module tests the effects of noise on the convergence. """
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from src.run_model_utils import run_simulations_until_convergence1
